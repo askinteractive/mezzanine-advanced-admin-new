5645f291010d41522522af5b5b3d579c4e4494b7
Aymeric Bois <abois@askin.fr>
2015-09-23 00:17:41 +0200

    Fix nested sortable

--- mezzanine_advanced_admin/static/admin/js/page_tree.js
+++ mezzanine_advanced_admin/static/admin/js/page_tree.js
@@ -26,8 +26,8 @@ jQuery(document).ready(function($) {
     };
 
     function showButtonWithChildren() {
-        $('li:has(ol) .tree-toggle').css({visibility: 'visible'});
-        $('li:not(:has(ol)) .tree-toggle').css({visibility: 'hidden'});
+        $('ol:has(li.page)').parent().find('.tree-toggle').css({visibility: 'visible'});
+        $('ol:not(:has(li.page))').parent().find('.tree-toggle').css({visibility: 'hidden'});
     }
 
     showButtonWithChildren();
@@ -36,9 +36,9 @@ jQuery(document).ready(function($) {
         // Show/hide the branch and toggle the icon.
         var pageLink = $(this);
         pageLink.parents('li:first').find('ol:first').toggle();
-        pageLink.find('.page-tree-icon').toggle();
+        pageLink.find('.icon').toggle();
         // Add or remove the page ID from the cookie.
-        var opened = pageLink.find('.close:visible').length == 1;
+        var opened = pageLink.find('.tree-close:visible').length == 1;
         var id = pageLink.attr('id').split('-')[1];
         toggleID(opened, id);
         return false;
@@ -50,8 +50,8 @@ jQuery(document).ready(function($) {
         $('#page-' + ids.split(',').join(', #page-')).each(function(){
             var pageLink = $(this);
             pageLink.parents('li:first').find('ol:first').toggle();
-            pageLink.find('.page-tree-close').show();
-            pageLink.find('.page-tree-open').hide();
+            pageLink.find('.tree-close').show();
+            pageLink.find('.tree-open').hide();
         });
     }
 
@@ -75,40 +75,13 @@ jQuery(document).ready(function($) {
 
     // AJAX callback that's triggered when dragging a page to re-order
     // it has ended.
-    //var updateOrdering = function(event, ui) {
-    //    var parent = ui.item.parents('li:first');
-    //
-    //    var args = {
-    //        id: ui.item[0].id,
-    //        parent_id: parent.length ? parent[0].id : "null",
-    //        siblings: ui.item.parent().children().map(function(index, elem) {
-    //            return elem.id;
-    //        }).get()
-    //    };
-    //
-    //    $.post(window.__page_ordering_url, args, function(data) {
-    //        if (String(data).substr(0, 2) !== "ok") {
-    //            location.reload();
-    //        } else {
-    //            $(".messagelist").remove();
-    //        }
-    //    });
-    //
-    //    showButtonWithChildren();
-    //};
-
-    var updateOrdering = function ($item, container, _super, event) {
-        /**/
-        $item.removeClass(container.group.options.draggedClass).removeAttr("style");
-        $("body").removeClass(container.group.options.bodyClass);
-
-        /* Send ajax */
-        var parent = $item.parents('li:first');
+    var updateOrdering = function(event, ui) {
+        var parent = ui.item.parents('li:first');
 
         var args = {
-            id: $item[0].id,
+            id: ui.item[0].id,
             parent_id: parent.length ? parent[0].id : "null",
-            siblings: $item.parent().children().map(function(index, elem) {
+            siblings: ui.item.parent().children().map(function(index, elem) {
                 return elem.id;
             }).get(),
             csrfmiddlewaretoken: window.__csrf_token
@@ -122,34 +95,56 @@ jQuery(document).ready(function($) {
             }
         });
 
-        /**/
         showButtonWithChildren();
     };
+
+    //var updateOrdering = function ($item, container, _super, event) {
+    //    /**/
+    //    $item.removeClass(container.group.options.draggedClass).removeAttr("style");
+    //    $("body").removeClass(container.group.options.bodyClass);
+    //
+    //    /* Send ajax */
+    //    var parent = $item.parents('li:first');
+    //
+    //    var args = {
+    //        id: $item[0].id,
+    //        parent_id: parent.length ? parent[0].id : "null",
+    //        siblings: $item.parent().children().map(function(index, elem) {
+    //            return elem.id;
+    //        }).get(),
+    //        csrfmiddlewaretoken: window.__csrf_token
+    //    };
+    //
+    //    $.post(window.__page_ordering_url, args, function(data) {
+    //        if (String(data).substr(0, 2) !== "ok") {
+    //            location.reload();
+    //        } else {
+    //            $(".messagelist").remove();
+    //        }
+    //    });
+    //
+    //    /**/
+    //    showButtonWithChildren();
+    //};
 
     // Make the pages sortable via drag and drop.
     // The `connectWith` option needs to be set separately to get
     // around a performance bug with `sortable`.
-    $('#tree > ol').sortable({
+    var $tree = $('#tree > ol').nestedSortable({
         handle: '.ordering',
-        onDrop: updateOrdering,
-        placeholderClass: "tree-placeholder",
-        placeholder: '<li class="tree-placeholder"></li>',
-        nested: true,
-        vertical: true,
-        tolerance: 10
-        //itemSelector: 'li',
-        //toleranceElement: '> div',
-        //opacity: 0.5,
-        //stop: updateOrdering,
-        //forcePlaceholderSize: true,
-        //placeholder: 'placeholder',
-        //revert: 150,
-        //helper: 'clone',
-        //tabSize: 25,
-        //tolerance: 'pointer',
-        //isTree: true,
-        //expandOnHover: 1000,
-        //startCollapsed: true
+        opacity: 0.5,
+        stop: updateOrdering,
+        forcePlaceholderSize: true,
+        placeholder: 'placeholder',
+        revert: 150,
+        helper: 'clone',
+        items: 'li.page',
+        tabSize: 25,
+        tolerance: 'pointer',
+        toleranceElement: '> div',
+        isTree: true,
+        expandOnHover: 1000,
+        startCollapsed: true
     });
 
 });
