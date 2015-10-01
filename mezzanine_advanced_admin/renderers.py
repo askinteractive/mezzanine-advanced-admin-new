a064f1bb797a4799d88bf788c8b5eb3e466ea20b
Aymeric Bois <abois@askin.fr>
2015-10-01 14:38:17 +0200

    Fix radioselect design

--- mezzanine_advanced_admin/renderers.py
+++ mezzanine_advanced_admin/renderers.py
@@ -1,11 +1,14 @@
 from __future__ import absolute_import
+import re
 
 from django.contrib.auth.forms import ReadOnlyPasswordHashWidget
 from django.contrib.admin.widgets import (AdminDateWidget, AdminTimeWidget,
                                           AdminSplitDateTime, RelatedFieldWidgetWrapper)
-from django.forms import (FileInput, CheckboxInput, RadioSelect, CheckboxSelectMultiple)
+from django.forms import (FileInput, CheckboxInput, RadioSelect, CheckboxSelectMultiple, ClearableFileInput)
 
 from bootstrap3 import renderers
+from django.forms.extras import SelectDateWidget
+
 try:
     from bootstrap3.utils import add_css_class
 except ImportError:
@@ -27,6 +30,7 @@ class BootstrapFieldRenderer(renderers.FieldRenderer):
         html = self.field.as_widget(attrs=self.widget.attrs)
         # Start post render
         html = self.wrap_widget(html)
+        html = self.post_widget_render(html)
         return html
 
     def wrap_widget(self, html):
@@ -36,6 +40,36 @@ class BootstrapFieldRenderer(renderers.FieldRenderer):
                 klass=checkbox_class,
                 content=html,
             )
+        return html
+
+    def post_widget_render(self, html):
+        if isinstance(self.widget, RadioSelect):
+            html = self.list_to_class(html, 'radio')
+            html = self.fix_radio_select_input(html)
+        elif isinstance(self.widget, CheckboxSelectMultiple):
+            html = self.list_to_class(html, 'checkbox')
+        elif isinstance(self.widget, SelectDateWidget):
+            html = self.fix_date_select_input(html)
+        elif isinstance(self.widget, ClearableFileInput):
+            html = self.fix_clearable_file_input(html)
+        return html
+
+    def fix_radio_select_input(self, html):
+        def wrap(m):
+            return '<input ' + m.group(1) +'/><span class="circle"></span><span class="check"></span>'
+        html = re.sub(r'<input (.*)/>', wrap, html)
+        return html
+
+    def list_to_class(self, html, klass):
+        classes = add_css_class(klass, self.get_size_class())
+        mapping = [
+            ('<ul', '<div'),
+            ('</ul>', '</div>'),
+            ('<li', '<div class="{klass} {klass}-material-blue-700"'.format(klass=classes)),
+            ('</li>', '</div>'),
+        ]
+        for k, v in mapping:
+            html = html.replace(k, v)
         return html
 
     def add_class_attrs(self, widget=None):
@@ -56,8 +90,9 @@ class BootstrapFieldRenderer(renderers.FieldRenderer):
                                  RelatedFieldWidgetWrapper)):
             # for some admin widgets we don't want the input to take full horizontal space
             classes = add_css_class(classes, 'form-control form-control-inline', prepend=True)
+        elif isinstance(widget, RadioSelect):
+            classes = add_css_class(classes, 'radioselect', prepend=True)
         elif not isinstance(widget, (CheckboxInput,
-                                     RadioSelect,
                                      CheckboxSelectMultiple,
                                      FileInput)):
             classes = add_css_class(classes, 'form-control', prepend=True)
